8723e94e04c34ecaeca88b415506dfca67c9e60d
Anil1415 <91272343+Anil1415@users.noreply.github.com>
2022-12-23 13:08:09 -0700

    Add files via upload
    This assignment describes about boolean, for loop, range function. And it also includes module file name 'spam.py' for the last question for import a function called 'bacon'.

---
+++ spam.py
@@ -0,0 +1,14 @@
+def bacon():
+    
+
+    spam = 6
+
+
+    if spam == 10:
+        print('eggs')
+    if spam > 5:
+        print('bacon')
+    else:
+        print('ham')
+        print('spam')
+        print('spam')
